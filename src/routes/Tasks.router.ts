3894503445e7e4ded12a7b275e376bd0c71451b3
St-thomas-Aquinas <maxkuria254@gmail.com>
2025-07-29 15:45:31 +0300

    End point for Login User

--- src/routes/Tasks.router.ts
+++ src/routes/Tasks.router.ts
@@ -1,14 +1,7 @@
 //import Declarations
 import { Router } from "express";
-import { CreateNewTask } from "../controllers/Task.controller";
-import { GetTask } from "../controllers/Task.controller";
-import { DeletTask } from "../controllers/Task.controller";
-import { updateTask } from "../controllers/Task.controller";
-import { RestoretTask } from "../controllers/Task.controller";
-import { GetDeletedTask } from "../controllers/Task.controller";
-import { GetSpecificTask } from "../controllers/Task.controller";
-import { GetAllTask } from "../controllers/Task.controller";
-import { CompleteTask} from "../controllers/Task.controller";
+//import { logedIn } from "../controllers/Task.controller";
+
 //End of Import Declarations
 
 //Variables Declaraions
@@ -16,14 +9,6 @@ const TaskRouter = Router();
 //End of Variables Declaraions
 
 //Routers
-TaskRouter.post("/", CreateNewTask);
-TaskRouter.get("/NotDelete/:Mytasks", GetTask);
-TaskRouter.get("/all", GetAllTask);
-TaskRouter.delete("/de/:Delete", DeletTask);
-TaskRouter.delete("/re/:restore", RestoretTask);
-TaskRouter.patch("/update/:update", updateTask);
-TaskRouter.get("/Trash/:Mytrash", GetDeletedTask);
-TaskRouter.get("/SpesificTask/:Taskid", GetSpecificTask);
-TaskRouter.delete("/MarkCompletedTask/:Taskid", CompleteTask);
-//End of Router
+//TaskRouter.post("/", CreateNewTask);
+
 export default TaskRouter;
--- src/routes/User.router.ts
+++ src/routes/User.router.ts
@@ -1,10 +1,8 @@
 //import Declarations
 import { Router } from "express";
 import { CreateUser } from "../controllers/User.controller";
-import { GetAllUsers } from "../controllers/User.controller";
-import { LoginUser } from "../controllers/User.controller";
-import { updateUser } from "../controllers/User.controller";
-import { GetSpecificUser1 } from "../controllers/User.controller";
+import { login } from "../controllers/User.controller";
+
 //End of Import Declarations
 
 //Variables Declaraions
@@ -13,10 +11,7 @@ const UserRouter = Router();
 
 //Routers
 UserRouter.post("/api/auth/register/", CreateUser);
-UserRouter.get("/", GetAllUsers);
-UserRouter.get("/api/auth/login/:Login", LoginUser);
-UserRouter.get("/specific/:username", GetSpecificUser1);
-UserRouter.patch("/api/auth/update/:username", updateUser);
+UserRouter.post("/api/auth/login/", login);
 
 //End of Router
 export default UserRouter;
